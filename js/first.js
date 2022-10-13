fca54eea0e0cbd3c7556249ca6bf83a5e2dcf18b
sarwan4553 <59577070+sarwan4553@users.noreply.github.com>
2022-10-14 01:51:24 +0530

    update the first js file

--- js/first.js
+++ js/first.js
@@ -1,2 +1,24 @@
-alert("hello");
-console.log("hello")
+// "use strict";
+// alert(1 +
+//         2 +
+//         3)
+// console.log("hello")
+
+
+// let, var, const
+//let =work on locally
+//var =work on globle
+// const = value is constant
+
+// decleartion          name, age, skill, data
+// assignment           radha, 26, devloper, nathing else
+
+// const name = "radha";
+// name = "reena"
+
+let name = "sarwan";
+
+// var name = "radha";
+// var name = "reena"
+// var name = "beena"
+// console.log(name)
