1adf17b6eadf8bd0462415fdb03643c72612d690
sarwan4553 <59577070+sarwan4553@users.noreply.github.com>
2022-10-23 00:44:21 +0530

    second class

---
+++ js/second.js
@@ -0,0 +1,57 @@
+/**
+ * primitive data type
+ * number
+ * string
+ * boolean
+ * null
+ * undifiend
+ * 
+ * non-primetive data type
+ * array
+ * object
+ * funciton
+ */
+//============= number
+// var age = 28;
+// let price = 23.99;
+// let amount = Number(age)
+// console.log("age:-", age);
+// console.log(price);
+// console.log(amount);
+// console.log(typeof age)
+
+// ==============string--------"", '', ``
+// let name = "reena";
+// var name2 = 'radha';
+// let name3 = `beena`
+// console.log('helo "reena"');
+// console.log("helo 'reena'");
+// console.log("jh dflkjsadfkjhafkdjhf" +
+//     "kajsdhf klajsdhfkj ahsdflkjhalsdkjfhl" +
+//     "kjdafhlkasdj hflkjasdhfl kahsdflkjh");
+
+
+// console.log(`hii.... ${name},
+//             how are ${name2}
+//             hello ${name3}
+//             ${45+8}
+//             `);
+
+// boolean------ true / false
+// let age = true;
+// console.log(age <= 10)
+
+//undifiend
+// let a;
+// let b = null
+// b = { name: "sarwan" }
+// console.log(typeof a)
+// console.log(typeof b.name)
+// console.log(b.name)
+
+// function demo() {
+//     a = 10;
+//     console.log("innter", a)
+// }
+// demo()
+// console.log("outer", a)
